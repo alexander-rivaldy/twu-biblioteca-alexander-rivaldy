e05b4300062d1655b2d4287c81d2ecbc028c24c8
Alex <s3466521@student.rmit.edu.au>
2019-02-02 17:14:36 +1100

    More commenting

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,6 +2,10 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
+    /**
+     * Main function to run the whole application
+     * @param args
+     */
     public static void main(String[] args) {
         MainMenu menu = new MainMenu();
         menu.run();
